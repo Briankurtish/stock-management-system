ef37979542e03360f1fbe09be2255cced8aa11f6
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-02-11 21:32:14 +0100

    customize admin panel
    changed the way the admin panel looked for the stock model

--- venv/src/stockmgmt/admin.py
+++ venv/src/stockmgmt/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Stock
+
+from .forms import StockCreateForm
 # Register your models here.
 
+from .models import Stock
 
-admin.site.register(Stock)
+class StockCreateAdmin(admin.ModelAdmin):
+   list_display = ['category', 'item_name', 'quantity']
+   form = StockCreateForm
+   list_filter = ['category']
+   search_fields = ['category', 'item_name']
+
+admin.site.register(Stock, StockCreateAdmin)
